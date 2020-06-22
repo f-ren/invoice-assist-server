a928243d5ccbe3cfe19f18d554dbbee623fa2d27
Ren F <renfinn89@gmail.com>
2020-06-22 15:24:44 -0700

    updates env vars to heroku

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,6 @@
+require('dotenv').config();
 const { expect } = require('chai');
 const supertest = require('supertest');
-
-process.env.TEST_DB_URL = process.env.TEST_DB_URL;
 
 global.expect = expect;
 global.supertest = supertest;
